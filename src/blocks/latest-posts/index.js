605e06d160e9003874ffa51144a5936416428993
joemeers82 <joemeers82@gmail.com>
2021-01-18 20:32:46 -0800

    started new block

---
+++ src/blocks/latest-posts/index.js
@@ -0,0 +1,15 @@
+import edit from './edit';
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+registerBlockType('cp-blocks/latest-posts', {
+	title: __('Latest Posts', 'cp-blocks'),
+	description: __('Block Showing Latest Posts', 'cp-blocks'),
+	icon: 'admin-post',
+	category: 'cp_blocks-category',
+	edit:edit,
+
+	save() {
+		return null;
+	}
+});
